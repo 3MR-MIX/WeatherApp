9063dfdfb2c0cf36a2c8df0d4429765a31503e67
amr.khaled <amr.khaled@SIS-SE-AKHALID>
2025-04-07 13:06:29 +0200

    Add project files.

---
+++ Entity/City.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApp.Entity
+{
+    public class City
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } 
+        [Required]
+        public string Country { get; set; } 
+        public IList<Forecast> Forecasts { get; set; }
+    }
+}
---
+++ Repos/CityRepo/ICityRepo.cs
@@ -0,0 +1,13 @@
+﻿using WeatherApp.DTOs.City;
+
+namespace WeatherApp.Repos.CityRepo
+{
+    public interface ICityRepo
+    {
+        List<CityResponseDto> GetAllCities();
+        CityResponseDto GetCityById(int id);
+        bool AddCity(CityRequestDto cityRequestDto);
+
+
+    }
+}
